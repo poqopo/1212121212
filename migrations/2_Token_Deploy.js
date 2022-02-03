59d939fa0463a61a88a660976c78f9dda381df42
poqopo <tkdwns63@yonsei.ac.kr>
2022-02-03 13:26:54 +0900

    can mint

--- migrations/2_Token_Deploy.js
+++ migrations/2_Token_Deploy.js
@@ -1,15 +1,16 @@
 //const We_Made_Future = artifacts.require('We_Made_Future')
 //const WUSDStablecoin = artifacts.require('WUSDStablecoin')
-//const WUSDPool = artifacts.require('WUSDPool')
-//const WUSDPoolLibrary = artifacts.require('WUSDPoolLibrary')
+const WUSDPool = artifacts.require('WUSDPool')
+const WUSDPoolLibrary = artifacts.require('WUSDPoolLibrary')
 //const ChainlinkETHUSDPriceConsumer = artifacts.require('ChainlinkETHUSDPriceConsumer')
 //const Farm = artifacts.require("WMFChef")
 //const Stake = artifacts.require("WMFstake")
 const WMF_WETH = artifacts.require('UniswapPairOracle_WMF_WETH')
-
+const WUSD_WETH = artifacts.require('UniswapPairOracle_WUSD_WETH')
+const MDAI_WETH = artifacts.require('UniswapPairOracle_MDAI_WETH')
 
 const Owner_Address = "0xe7f890A0CB4c0Cbd68848F26F0998562502323Dc"
-const Collateral_Address = "0x628712ee7f0c240c24820af6Efa06CAD3DA8AC20"
+const Collateral_Address = "0x0feB3a8702F4943aA3e502369fEda55258f1a018"
 
 module.exports = async function(deployer, network, accounts) {
   // Deploy WUSD
@@ -21,9 +22,9 @@ module.exports = async function(deployer, network, accounts) {
 //  const we_made_future = await We_Made_Future.deployed()
 
   // Deploy WUSDPool
-//  await deployer.deploy(WUSDPoolLibrary)
-//  await deployer.link(WUSDPoolLibrary, WUSDPool)
-//  await deployer.deploy(WUSDPool, "0xB7FB1bb06a10f7Ec53CaC0Bf29d7d7c058A47dE8", "0x040d87e9A4f44dF98b67C60B47515019975E7ad7", "0x628712ee7f0c240c24820af6Efa06CAD3DA8AC20", Owner_Address)
+  await deployer.deploy(WUSDPoolLibrary)
+  await deployer.link(WUSDPoolLibrary, WUSDPool)
+  await deployer.deploy(WUSDPool, "0xB7FB1bb06a10f7Ec53CaC0Bf29d7d7c058A47dE8", "0x040d87e9A4f44dF98b67C60B47515019975E7ad7", "0x0feB3a8702F4943aA3e502369fEda55258f1a018", Owner_Address)
 
   // Deploy Farm
 //  await deployer. deploy(Farm, "0xe7cB1d8902B372199ae07776Ac037207CBe1d6AA",  "0xe7f890A0CB4c0Cbd68848F26F0998562502323Dc", "0x0380bb57500562DA6Da4789AE9D04669c28243F1", 5000000000000, 11871940)
@@ -31,6 +32,8 @@ module.exports = async function(deployer, network, accounts) {
 
 //  await deployer.deploy(Stake, "0xe7cB1d8902B372199ae07776Ac037207CBe1d6AA", "0xe7f890A0CB4c0Cbd68848F26F0998562502323Dc", 10000000000, 11895500)
 //  await deployer.deploy(ChainlinkETHUSDPriceConsumer)
-  await deployer.deploy(WMF_WETH, "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f", "0xB7FB1bb06a10f7Ec53CaC0Bf29d7d7c058A47dE8", "0xc778417E063141139Fce010982780140Aa0cD5Ab", "0xe7f890A0CB4c0Cbd68848F26F0998562502323Dc")
+  await deployer.deploy(WMF_WETH, "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f", "0x040d87e9A4f44dF98b67C60B47515019975E7ad7", "0xc778417E063141139Fce010982780140Aa0cD5Ab", "0xe7f890A0CB4c0Cbd68848F26F0998562502323Dc")
+  await deployer.deploy(WUSD_WETH, "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f", "0xB7FB1bb06a10f7Ec53CaC0Bf29d7d7c058A47dE8", "0xc778417E063141139Fce010982780140Aa0cD5Ab", "0xe7f890A0CB4c0Cbd68848F26F0998562502323Dc")
+  await deployer.deploy(MDAI_WETH, "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f", "0x0feB3a8702F4943aA3e502369fEda55258f1a018", "0xc778417E063141139Fce010982780140Aa0cD5Ab", "0xe7f890A0CB4c0Cbd68848F26F0998562502323Dc")
 
 }
